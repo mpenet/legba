96996b9eee670e387c8c8a34fed013af6cc8e386
Max Penet <mpenet@s-exp.com>
2025-10-07 09:20:37 +0200

    refactor(json_schema_test): remove bang from `validate!` function calls
    Replace `js/validate!` with `js/validate` to align with updated
    function naming convention, making the code more consistent and
    readable. No changes to logic or behavior were introduced.

--- test/s_exp/json_schema_test.clj
+++ test/s_exp/json_schema_test.clj
@@ -9,17 +9,17 @@
     (testing "Valid input passes validation"
       (let [valid1 "{\"fruits\":[\"apple\",\"banana\"],\"vegetables\":[{\"veggieName\":\"potato\",\"veggieLike\":true}]}"
             valid2 "{\"fruits\":[],\"vegetables\":[]}"]
-        (is (nil? (js/validate! schema valid1)))
-        (is (nil? (js/validate! schema valid2)))))
+        (is (nil? (js/validate schema valid1)))
+        (is (nil? (js/validate schema valid2)))))
     (testing "Missing required veggie fields triggers error"
       (let [invalid "{\"fruits\":[],\"vegetables\":[{\"veggieLike\":true}]}"
-            errors (js/validate! schema invalid)]
+            errors (js/validate schema invalid)]
         (is (coll? errors))
         (is (seq errors))
         (is (some #(= (:type %) "required") errors))))
     (testing "Wrong type errors for arrays or objects"
       (let [invalid "{\"fruits\": [1,2,3], \"vegetables\": []}"
-            errors (js/validate! schema invalid)]
+            errors (js/validate schema invalid)]
         (is (coll? errors))
         (is (seq errors))
         (is (some #(= (:type %) "type") errors))))))
@@ -28,7 +28,7 @@
   (let [schema (js/schema schema-path)]
     (testing "Extracts error info"
       (let [invalid "{\"vegetables\":[{\"veggieLike\":false}]}"
-            errors (js/validate! schema invalid)]
+            errors (js/validate schema invalid)]
         (is (every? #(contains? % :type) errors))
         (is (every? #(contains? % :path) errors))
         (is (every? #(contains? % :message) errors))))))
