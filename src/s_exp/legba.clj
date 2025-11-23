7511ddf63e8b4bc2e326c8434d2ad25150757a15
Chuck Cassel <chuck.cassel@gmail.com>
2025-11-23 16:08:38 -0500

    fix: filter by known http methods

--- src/s_exp/legba.clj
+++ src/s_exp/legba.clj
@@ -21,7 +21,7 @@
   (let [missing-handlers
         (for [[path methods] (get openapi-schema "paths")
               [method & _] methods
-              :when (not (#{"description" "parameters" "servers" "summary"} method))
+              :when (#{"delete" "get" "head" "options" "patch" "post" "put" "trace"} method)
               :let [method (keyword method)
                     matching-entry (get openapi-handlers [(keyword method) path])]
               :when (not matching-entry)]
